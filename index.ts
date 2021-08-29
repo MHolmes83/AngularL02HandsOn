da4fdb26c82b442322e3caba2b851a2dbd9aa182
Morgaan Holmes <morgaan.holmes@my.scitexas.edu>
2021-08-29 16:25:29 -0400

    More Typescript
    Expanded on the principles of the last lesson and by creating the person class and using the extends value to create a new entry and also sorted an array of strings.

---
+++ index.ts
@@ -0,0 +1,30 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+    birthday: Date;
+}
+
+class Entry implements Person {
+    firstName: string;
+    lastName: string;
+    birthday: Date;
+
+    constructor(firstName: string, lastName: string, birthday: Date) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+    }
+}
+
+let newPerson = new Entry('Joyce', 'Clay', new Date('9/04/1935'))
+
+console.log('First Name: ' + newPerson.firstName + ', Last Name: ' + newPerson.lastName + ', Birthday: ' + newPerson.birthday);
+
+let numbers:Array<string> = ['twelve', 'one-hundred', 'sixteen', 'forty-four', 'seventy-three', 'three-million'];
+
+function sortArray<S>(array: S[]): S[] {
+    return array.sort();
+}
+
+console.log('Array before sorting: ' + numbers);
+console.log('Array after sorting: ' + sortArray(numbers));
